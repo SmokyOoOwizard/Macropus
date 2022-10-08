41597c6440888b468cdb66789ea5b2c06383c341
smokyOoOwizard <smoky-santa-claus@yandex.ru>
2022-10-08 22:27:01 +0200

    Add TestsWrapper for capture Console output; Add TestsWithSystem for ECS tests

---
+++ Tests/Utils/ECS/TestsWithSystems.cs
@@ -0,0 +1,42 @@
+﻿using Macropus.ECS;
+using Macropus.ECS.ComponentsStorage;
+using Macropus.ECS.ComponentsStorage.Impl;
+using Macropus.ECS.System;
+using Xunit.Abstractions;
+
+namespace Tests.Utils.ECS;
+
+public abstract class TestsWithSystems : TestsWrapper
+{
+	private readonly SystemsExecutor executor;
+
+	public readonly IClearableComponentsStorage ComponentsStorage;
+	public readonly IClearableComponentsStorage ChangesComponents;
+
+	public TestsWithSystems(ITestOutputHelper output) : base(output)
+	{
+		ComponentsStorage = new ComponentsStorage();
+		ChangesComponents = new ComponentsStorage();
+
+		// ReSharper disable once VirtualMemberCallInConstructor
+		executor = new SystemsExecutor(GetSystems());
+	}
+
+	public abstract ASystem[] GetSystems();
+
+	// ReSharper disable once InconsistentNaming
+	public void ExecuteSystems()
+	{
+		executor.Execute(
+			ComponentsStorage,
+			ComponentsStorage.GetEnumerationComponents(true),
+			ChangesComponents);
+	}
+
+
+	[Fact]
+	public void EmptyExecuteTest()
+	{
+		ExecuteSystems();
+	}
+}
