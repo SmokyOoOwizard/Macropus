19f3242ee4c1f7605fdd8890775ad03155aa601f
smokyOoOwizard <smoky-santa-claus@yandex.ru>
2022-10-10 20:10:59 +0200

    Fix tests folder structure

---
+++ Tests/FileSystem/FileSystemProviderTests.cs
@@ -0,0 +1,21 @@
+﻿using Macropus.FileSystem.Impl;
+using Tests.Utils;
+using Xunit.Abstractions;
+
+namespace Tests.FileSystem;
+
+public class FileSystemProviderTests : TestsWithFiles
+{
+	public FileSystemProviderTests(ITestOutputHelper output) : base(output) { }
+
+
+	[Fact]
+	public async void CreateProviderTest()
+	{
+		var fsProvider = await FileSystemProvider.Create(ExecutePath).ConfigureAwait(false);
+
+		Assert.NotNull(fsProvider);
+
+		fsProvider.Dispose();
+	}
+}
---
+++ Tests/Project/Storage/LocalStorageTests.cs
@@ -0,0 +1,52 @@
+using Macropus.Project.Storage.Impl;
+using Tests.Utils;
+using Xunit.Abstractions;
+
+namespace Tests.Project.Storage;
+
+public class LocalStorageTests : TestsWithProjectsStorage
+{
+	private const string PROJECT_NAME = "CreateTest";
+
+	public LocalStorageTests(ITestOutputHelper output) : base(output) { }
+
+	[Fact]
+	public async void CreateByNameTest()
+	{
+		var projectId = await ProjectStorage
+			.CreateProjectAsync(new ProjectCreationInfo { Name = PROJECT_NAME })
+			.ConfigureAwait(false);
+
+		Assert.NotEqual(Guid.Empty, projectId);
+	}
+
+	[Fact]
+	public async void CreateByPathTest()
+	{
+		const string ADDITIONAL_PATH = "Additional";
+
+		var path = Path.Combine(ExecutePath, ADDITIONAL_PATH);
+
+		var projectId = await ProjectStorage
+			.CreateProjectByPathAsync(path, new ProjectCreationInfo { Name = PROJECT_NAME })
+			.ConfigureAwait(false);
+
+		Assert.NotEqual(Guid.Empty, projectId);
+	}
+
+	[Fact]
+	public async void CreateByExistsPathTest()
+	{
+		const string ADDITIONAL_PATH = "Additional";
+
+		var path = Path.Combine(ExecutePath, ADDITIONAL_PATH);
+
+		Directory.CreateDirectory(path);
+
+		var projectId = await ProjectStorage
+			.CreateProjectByPathAsync(path, new ProjectCreationInfo { Name = PROJECT_NAME })
+			.ConfigureAwait(false);
+
+		Assert.NotEqual(Guid.Empty, projectId);
+	}
+}
