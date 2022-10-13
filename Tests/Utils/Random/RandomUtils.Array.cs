1faaaddc27eb9658e0dd51a25b549d5136d22a3c
smokyOoOwizard <smoky-santa-claus@yandex.ru>
2022-10-13 20:22:31 +0200

    Add new tests utils

---
+++ Tests/Utils/Random/RandomUtils.Array.cs
@@ -0,0 +1,17 @@
+﻿// ReSharper disable StringLiteralTypo
+
+namespace Tests.Utils.Random;
+
+public static partial class RandomUtils
+{
+	public static byte[] GetRandomByteArray(int lenght)
+	{
+		var buffer = new byte[lenght];
+		var random = new System.Random();
+
+		for (var i = 0; i < buffer.Length; i++)
+			buffer[i] = (byte)random.Next(256);
+
+		return buffer;
+	}
+}
---
+++ Tests/Utils/Random/RandomUtils.String.cs
@@ -0,0 +1,34 @@
+﻿namespace Tests.Utils.Random;
+
+public static partial class RandomUtils
+{
+	public static string GetRandomStringOfBase64(int lenght)
+	{
+		const string CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+		return GetRandomString(lenght, CHARS);
+	}
+
+	public static string GetRandomString(int lenght)
+	{
+		const string CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+		var stringChars = new char[lenght];
+		var random = new System.Random();
+
+		for (var i = 0; i < stringChars.Length; i++)
+			stringChars[i] = CHARS[random.Next(CHARS.Length)];
+
+		return new string(stringChars);
+	}
+
+	public static string GetRandomString(int lenght, string availableChars)
+	{
+		var stringChars = new char[lenght];
+		var random = new System.Random();
+
+		for (var i = 0; i < stringChars.Length; i++)
+			stringChars[i] = availableChars[random.Next(availableChars.Length)];
+
+		return new string(stringChars);
+	}
+}
