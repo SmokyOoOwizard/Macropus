52a6477b4fbc99aff707f9ea71c24967d816acf1
smokyOoOwizard <smoky-santa-claus@yandex.ru>
2022-11-02 07:06:25 +0200

    Add log file in tests wrapper

--- Tests/Utils/ConsoleConverter.cs
+++ Tests/Utils/ConsoleConverter.cs
@@ -6,21 +6,39 @@ namespace Tests.Utils;
 public class ConsoleConverter : TextWriter
 {
 	private readonly ITestOutputHelper output;
+	private readonly string logFile;
 
-	public ConsoleConverter(ITestOutputHelper output)
+	private TextWriter? fileWriter;
+
+	public ConsoleConverter(ITestOutputHelper output, string logFile)
 	{
 		this.output = output;
+		this.logFile = logFile;
 	}
 
 	public override Encoding Encoding => Encoding.Default;
 
 	public override void WriteLine(string? message)
 	{
+		if (fileWriter == null)
+		{
+			fileWriter = new StreamWriter(new FileStream(logFile, FileMode.Append, FileAccess.Write));
+		}
+
+		fileWriter.WriteLine(message);
+		fileWriter.Flush();
 		output.WriteLine(message);
 	}
 
 	public override void WriteLine(string format, params object?[] args)
 	{
+		if (fileWriter == null)
+		{
+			fileWriter = new StreamWriter(new FileStream(logFile, FileMode.Append, FileAccess.Write));
+		}
+
+		fileWriter.WriteLine(format, args);
+		fileWriter.Flush();
 		output.WriteLine(format, args);
 	}
 
--- Tests/Utils/Tests/TestsWrapper.cs
+++ Tests/Utils/Tests/TestsWrapper.cs
@@ -1,15 +1,30 @@
-﻿using Xunit.Abstractions;
+﻿using System.Reflection;
+using Xunit.Abstractions;
 
 namespace Tests.Utils.Tests;
 
 public abstract class TestsWrapper
 {
+	public readonly string ExecutePath;
+	
 	protected readonly ITestOutputHelper Output;
-
+	
 	public TestsWrapper(ITestOutputHelper output)
 	{
 		Output = output;
-		var converter = new ConsoleConverter(output);
+		
+		var type = output.GetType();
+		var testMember = type.GetField("test", BindingFlags.Instance | BindingFlags.NonPublic);
+		var test = (ITest)testMember?.GetValue(output)!;
+
+		var testPrefix = test.TestCase.TestMethod.TestClass.Class.Name?.Replace('.', '\\') ?? string.Empty;
+
+		ExecutePath = Path.Combine(
+			AppDomain.CurrentDomain.BaseDirectory, "TestArtifacts", testPrefix,
+			test.DisplayName, Guid.NewGuid().ToString("N"));
+		
+		
+		var converter = new ConsoleConverter(output, Path.Combine(ExecutePath, "log.txt"));
 		Console.SetOut(converter);
 	}
 }
