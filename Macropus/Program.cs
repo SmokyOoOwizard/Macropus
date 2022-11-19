33e893f05489a100b133b04f6c3d6c0e5edc42e9
smokyOoOwizard <smoky-santa-claus@yandex.ru>
2022-11-19 15:06:50 +0200

    Fix host startup

--- Macropus/Program.cs
+++ Macropus/Program.cs
@@ -8,18 +8,20 @@ namespace Macropus;
 
 internal class Program
 {
+
 	private static async Task Main(string[] args)
 	{
 		var builder = Host.CreateDefaultBuilder(args);
 
 		builder.ConfigureLogging(b => b.ClearProviders());
 		builder.SetUpDelogger();
+		
 		builder.UseServiceProviderFactory(new AutofacServiceProviderFactory())
 			.ConfigureContainer<ContainerBuilder>(b => { b.RegisterModule<MacropusContainerBuilder>(); })
 			.UseConsoleLifetime();
 
 		var host = builder.Build();
 
-		await host.StartAsync();
+		await host.RunAsync();
 	}
 }
