331cdbae6e47e2aeb0a480e85330f8c96a1fd979
smokyOoOwizard <smoky-santa-claus@yandex.ru>
2023-05-13 02:21:23 +0200

    Add Odin projects

---
+++ Odin/Odin.Tests/UnitTest1.cs
@@ -0,0 +1,7 @@
+namespace Odin.Tests;
+
+public class UnitTest1
+{
+	[Fact]
+	public void Test1() { }
+}
---
+++ Odin/Odin/Class1.cs
@@ -0,0 +1,30 @@
+﻿namespace Odin;
+
+
+
+
+public interface IOdinContext : IDisposable, IAsyncDisposable
+{
+	/// <summary>
+	/// Load raw dependencies
+	/// </summary>
+	/// <param name="ctx"></param>
+	/// <returns></returns>
+	Task InitAsync(CancellationToken ctx = default);
+
+	/// <summary>
+	/// Initialize inner services and run it
+	/// </summary>
+	/// <param name="ctx"></param>
+	/// <returns></returns>
+	Task StartAsync(CancellationToken ctx = default);
+
+	/// <summary>
+	/// Stop inner services
+	///
+	/// You can just dispose context. stop method need only for reuse already initialized context
+	/// </summary>
+	/// <param name="ctx"></param>
+	/// <returns></returns>
+	Task StopAsync(CancellationToken ctx = default);
+}
