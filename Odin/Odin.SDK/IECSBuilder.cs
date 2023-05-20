89aefaf6db652097f2dac5d86e080edcfa8d2e74
smokyOoOwizard <smoky-santa-claus@yandex.ru>
2023-05-20 22:52:49 +0200

    Add module projects stub

---
+++ Odin/Odin.SDK/IECSBuilder.cs
@@ -0,0 +1,9 @@
+﻿using Macropus.ECS.Systems;
+
+namespace Odin.SDK;
+
+public interface IECSBuilder
+{
+	void RegisterSystem<T>(string contextName) where T : ISystem;
+	void RegisterSystem<T>(string contextName, int order) where T : ISystem;
+}
---
+++ Odin/Odin.SDK/Module.cs
@@ -0,0 +1,10 @@
+﻿using Autofac;
+
+namespace Odin.SDK;
+
+public abstract class Module
+{
+	public abstract void Configure(ContainerBuilder builder);
+
+	public virtual void ConfigureECS(IECSBuilder builder) { }
+}
